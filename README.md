9bee3bb5829d2186c37292ada253184526e1ba0f
ManojKudwe <kudvemsk@gmail.com>
2021-04-23 18:33:34 +0530

    Laravel project

--- README.md
+++
@@ -1,3 +0,0 @@
-# Training
-images folder is not included in assignment_2, assignment_3 and assignment_4.
-please use images folder in assignments
--- assignment_4/cate.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con = mysqli_connect('localhost', 'root', '','training');
- if (!$con)
-    {
-	 die('Could not connect: ' . mysql_error());
-	} 
-?>
